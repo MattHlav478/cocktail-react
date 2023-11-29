2f0c06dcc9d573e1f54682e36eea4051c5a9b1d1
M LavaT <mwhlavaty@gmail.com>
2023-11-29 21:38:48 +0700

    add fc1Name condition for city search

--- src/services/citySearch.js
+++ src/services/citySearch.js
@@ -5,11 +5,15 @@ export const citySearch = async (query) => {
     );
     const data = await response.json();
     console.log(data);
-    const cities = await data.geonames.map((city) => ({
-      city: city.name,
-      state: city.adminName1,
-      country: city.countryName,
-    }));
+    const cities = await data.geonames.map((city) => {
+      if (city.fc1Name = "city, village,...") {
+        return {
+          city: city.name,
+          state: city.adminName1,
+          country: city.countryName,
+        };
+      } else return null;
+    });
     return cities;
   } catch (error) {
     console.group("Error fetching city data: ", error);
